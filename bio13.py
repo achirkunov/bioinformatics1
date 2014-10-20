f37d0312923c8e5e2cd06ef4d2765eefa5612e3b
Anton Chirkunov <anton@wheely.com>
2014-10-20 17:17:07 +0400

    finished 13

--- bio13.py
+++ bio13.py
@@ -12,3 +12,18 @@ def testReverseComplement():
     lines = f.readlines()
     sequence = lines[0].rstrip()
     print(reverseComplement(sequence))
+    
+def occurences(pattern,genome):
+    o = []
+    for i in range(len(genome)-len(pattern)+1):
+        if genome[i:i+len(pattern)] == pattern:
+            o.append(i)
+    return o
+
+def testOccurences():
+    f = open("data/dataset_3_5.txt")
+    lines = f.readlines()
+    pattern = lines[0].rstrip()
+    genome = lines[1].rstrip()
+    output = occurences(pattern,genome)
+    print(" ".join(map(str, output)))
