714facf5ea58d9862ecf7c47818848e4fc3baa65
Anton Chirkunov <anton@wheely.com>
2014-10-20 17:18:27 +0400

    removed ol file

--- python/dna.py
+++
@@ -1,8 +0,0 @@
-def complement(input):
-    baseComplement = {'A':'T', 'C':'G', 'G':'C', 'T':'A'}
-    letters = list(input)
-    letters = [baseComplement[base] for base in letters]
-    return ''.join(letters)
-
-def reverseComplement(input):
-    return input[::-1]
